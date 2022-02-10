61ddb5497bbf8c5573119278aaef3c69c0d98de5
alesalg <allesalg@gmail.com>
2022-02-10 08:14:19 -0300

    add rotas produtos

--- routes/produtosRoutes.js
+++ routes/produtosRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-
+const ProdutoController = require("../controllers/ProdutoController")
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.render("home");
-});
+router.get("/produtos", ProdutoController.paginaProdutos);
+router.get("/produtos/novo", ProdutoController.paginaAdicionarProduto)
+
+router.post("/produtos/enviar", ProdutoController.addProduto);
 
 module.exports = router
